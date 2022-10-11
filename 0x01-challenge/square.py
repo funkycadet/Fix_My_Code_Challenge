fecd5519365490772afa3075286eb44af9992bc9
smith2eric <ericsmithsonian2@gmail.com>
2022-10-12 00:44:10 +0100

    [fix]: replaced width with height

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -15,7 +15,7 @@ class square:
         return self.width * self.width
 
     def PermiterOfMySquare(self):
-        return (self.width * 2) + (self.width * 2)
+        return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
         return "{}/{}".format(self.width, self.height)
